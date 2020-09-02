b68732f029a56df8ce30bb633b32d56e90d06a25
nokvai <bmeliseo@gmail.com>
2020-09-03 01:38:37 +0800

    third change

--- src/index.js
+++ src/index.js
@@ -44,4 +44,9 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
-require('update-electron-app')({repo: 'nokvai/autoupdatetest'})
+
+require('update-electron-app')({
+  repo: 'nokvai/autoupdatetest',
+  updateInterval: '5 minutes',
+  logger: require('electron-log')
+})
